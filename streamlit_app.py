a379daad133c82ca46875b8ffc59789cae066e96
ChHarding <charding@iastate.edu>
2024-06-12 21:16:02 +0000

    change app to mine

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,21 @@
-import altair as alt
-import numpy as np
-import pandas as pd
 import streamlit as st
 
+
+st.title("Stremlit Test")
+st.subheader("This is a subheader")
+st.write("This is very fine.")  
+st.markdown("[Streamlit](https://www.streamlit.io)") 
+html_page = """
+<div style="background-color:blue;padding:50px">
+<p style="color:yellow;font-size:50px">Enjoy Streamlit!</p>
+</div>
 """
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
-
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
-
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
-
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
-
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
-
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
+st.markdown(html_page, unsafe_allow_html=True)
+st.success("Success!")
+if st.button("Play"):
+      st.text("Hello world!")
+if st.checkbox("Checkbox"):
+      st.text("Checkbox selected") 
+city = st.selectbox("Your City", ["Napoli", "Palermo", "Catania"])
+occupation = st.multiselect("Your Occupation", ["Programmer", "Data Scientist", "IT Consultant", "DBA"])
+select_val = st.slider("Select a Value", 1, 10)
